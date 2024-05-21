61d3fc08fd96ae03fc3d91cd0b202fb1b26d0ef0
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-05-22 00:12:35 +0600

    initial project setup with db

---
+++ src/main/java/com/example/pathokdataloader/PathokDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathokdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathokDataLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathokDataLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pathokdataloader/PathokDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathokdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathokDataLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
