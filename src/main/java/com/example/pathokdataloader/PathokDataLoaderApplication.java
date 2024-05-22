d0470ecacaefa64bf7956753023aecb5c61870ce
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-05-22 23:40:11 +0600

    author data save method done

--- src/main/java/com/example/pathokdataloader/PathokDataLoaderApplication.java
+++ src/main/java/com/example/pathokdataloader/PathokDataLoaderApplication.java
@@ -3,9 +3,18 @@ package com.example.pathokdataloader;
 import com.example.pathokdataloader.author.Author;
 import com.example.pathokdataloader.author.AuthorRepository;
 import jakarta.annotation.PostConstruct;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class PathokDataLoaderApplication {
@@ -13,19 +22,64 @@ public class PathokDataLoaderApplication {
     @Autowired
     AuthorRepository authorRepository;
 
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
+
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
+
     public static void main(String[] args) {
         SpringApplication.run(PathokDataLoaderApplication.class, args);
+    }
+
+    private void initAuthors(){
+
+        Path path = Paths.get(authorDumpLocation);
+
+        try(Stream<String> lines = Files.lines(path)){
+
+            lines.forEach(line ->{
+
+                // Read and parse the line
+                String jsonString = line.substring(line.indexOf("{"));
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    // Construct Author object
+                    Author author = new Author();
+                    author.setName(jsonObject.optString("name"));
+                    author.setPersonalName(jsonObject.optString("personal_name"));
+                    author.setAuthorId(jsonObject.optString("key").replace("/authors/", ""));
+
+                    // Persist using repository
+                    authorRepository.save(author);
+
+                    System.out.println("Author saved named ::::::::: " + author.getName());
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            });
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void initWorks(){
+
     }
 
     @PostConstruct
     public void start(){
         System.out.println("Application Started");
-        Author author = new Author();
-        //author.setId("aaa");
-        author.setName("name");
-        author.setPersonalName("Last name");
+        initAuthors();
 
-        authorRepository.save(author);
+
     }
 
 }
